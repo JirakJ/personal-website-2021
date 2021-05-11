32fc6eb6125d048b3f9f766afa7b5da90aae1218
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-05-11 11:23:48 +0200

    Fix indentation

--- src/components/Resume/References.js
+++ src/components/Resume/References.js
@@ -8,9 +8,9 @@ const References = () => (
       <Link to="/contact">
         <h3>References are available upon request or check my</h3>
       </Link>
-        <h3>
-            <a href="https://www.linkedin.com/in/jirakj/">LinkedIn profile section recommendations</a>.
-        </h3>
+      <h3>
+        <a href="https://www.linkedin.com/in/jirakj/">LinkedIn profile section recommendations</a>.
+      </h3>
     </div>
   </div>
 );
