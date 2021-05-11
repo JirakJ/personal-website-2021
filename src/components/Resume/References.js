03b85a254eb7ed20ee1c80688062ebfb72e07ff2
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-05-11 11:15:32 +0200

    Fix bottom link

--- src/components/Resume/References.js
+++ src/components/Resume/References.js
@@ -6,8 +6,11 @@ const References = () => (
     <div className="link-to" id="references" />
     <div className="title">
       <Link to="/contact">
-        <h3>References are available upon request or check my <a href="https://www.linkedin.com/in/jirakj/">LinkedIn profile section recommendations</a>.</h3>
+        <h3>References are available upon request or check my</h3>
       </Link>
+        <h3>
+            <a href="https://www.linkedin.com/in/jirakj/">LinkedIn profile section recommendations</a>.
+        </h3>
     </div>
   </div>
 );
