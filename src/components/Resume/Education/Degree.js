19744d4a76e2917779a000c62569441020909ee5
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-05-12 08:41:50 +0200

    Add final thesis to data and to component

--- src/components/Resume/Education/Degree.js
+++ src/components/Resume/Education/Degree.js
@@ -5,6 +5,7 @@ const Degree = ({ data }) => (
   <article className="degree-container">
     <header>
       <h4 className="degree">{data.degree}</h4>
+      <span className="these"><b>Final thesis:</b> <a href={data.finalThesis.link}>{data.finalThesis.name}</a></span>
       <p className="school"><a href={data.link}>{data.school}</a>, {data.year}</p>
     </header>
   </article>
@@ -16,6 +17,11 @@ Degree.propTypes = {
     link: PropTypes.string.isRequired,
     school: PropTypes.string.isRequired,
     year: PropTypes.number.isRequired,
+    finalThesis: {
+      name: PropTypes.string.isRequired,
+      link: PropTypes.string.isRequired,
+      theseLink: PropTypes.string.isRequired,
+    },
   }).isRequired,
 };
 
--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -4,12 +4,22 @@ const degrees = [
     degree: 'Ing. from Communications and Informatics',
     link: 'https://vutbr.cz',
     year: 2017,
+    finalThesis: {
+      name: 'An Alternative JPEG CODER/DECODER',
+      link: 'https://www.vutbr.cz/studenti/zav-prace/detail/101969',
+      theseLink: 'https://www.vutbr.cz/www_base/zav_prace_soubor_verejne.php?file_id=147560',
+    },
   },
   {
     school: 'Brno University of Technology',
     degree: 'Bc. from Teleinformatics',
     link: 'https://vutbr.cz',
     year: 2015,
+    finalThesis: {
+      name: 'Visualization and Data Interpretation in Social Network',
+      link: 'https://www.vutbr.cz/studenti/zav-prace/detail/85217',
+      theseLink: 'https://www.vutbr.cz/www_base/zav_prace_soubor_verejne.php?file_id=105496',
+    },
   },
 ];
 
