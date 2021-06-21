6c281aab5bc9b48c1b3578c0e5a50bb7dd1d7697
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-06-21 07:51:18 +0200

    Fix layout

--- src/components/Projects/Techstack.js
+++ src/components/Projects/Techstack.js
@@ -2,12 +2,10 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const Techstack = ({ data }) => (
-  <div>
-    <h4>Techstack: </h4>
-    <p>
-      { data.techstack.map((item, index) => <Fragment key={`${Math.random() * 1000}`}>{ index > 0 && ', ' }{ item } </Fragment>) }
-    </p>
-  </div>
+  <p>
+    <b>Techstack: </b><br />
+    { data.techstack.map((item, index) => <Fragment key={`${Math.random() * 1000}`}>{ index > 0 && ', ' }{ item } </Fragment>) }
+  </p>
 );
 
 Techstack.propTypes = {
