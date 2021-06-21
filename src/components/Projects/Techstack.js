563a77dc287a484ade7ded47c39541d0db2588e0
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-06-21 07:47:03 +0200

    Fix layout H4 cannot be inside p tag Add key to each item

--- src/components/Projects/Techstack.js
+++ src/components/Projects/Techstack.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const Techstack = ({ data }) => (
-  <p>
-    <h4>Techstack: </h4>{ data.techstack.map((item, index) => <>{ index > 0 && ', ' }{ item } </>) }
-  </p>
+  <div>
+    <h4>Techstack: </h4>
+    <p>
+      { data.techstack.map((item, index) => <Fragment key={`${Math.random() * 1000}`}>{ index > 0 && ', ' }{ item } </Fragment>) }
+    </p>
+  </div>
 );
 
 Techstack.propTypes = {
