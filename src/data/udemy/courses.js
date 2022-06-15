46b261ec491e763264a0c7f28d41547ef458e48f
Ing. Jakub Jirák <jakub@jakubjirak.com>
2022-06-15 08:15:32 +0200

    Update courses.js

--- src/data/udemy/courses.js
+++ src/data/udemy/courses.js
@@ -1,67 +1,118 @@
 const courses = [{
-    "_class": "course",
-    "id": 4440262,
-    "title": "Python web scrapping of react.js page in the nut shell",
-    "url": "/course/python-web-scrapping-of-reactjs-page-in-the-nut-shell/",
-    "is_paid": true,
-    "price": "€39.99",
-    "price_detail": {"amount": 39.99, "currency": "EUR", "price_string": "€39.99", "currency_symbol": "€"},
-    "price_serve_tracking_id": "zoM1Cb33SLSogjtb2wBrbQ",
-    "visible_instructors": [{
-        "_class": "user",
-        "id": 57471282,
-        "title": "Jakub Jirák",
-        "name": "Jakub",
-        "display_name": "Jakub Jirák",
-        "job_title": "Freelance Full Stack Software Engineer and Instructor",
-        "image_50x50": "https://img-b.udemycdn.com/user/50x50/57471282_7681_4.jpg",
-        "image_100x100": "https://img-b.udemycdn.com/user/100x100/57471282_7681_4.jpg",
-        "initials": "JJ",
-        "url": "/user/jakub-jirak/"
-    }],
-    "image_125_H": "https://img-b.udemycdn.com/course/125_H/4440262_ebe6.jpg",
-    "image_240x135": "https://img-b.udemycdn.com/course/240x135/4440262_ebe6.jpg",
-    "is_practice_test_course": false,
-    "image_480x270": "https://img-b.udemycdn.com/course/480x270/4440262_ebe6.jpg",
-    "published_title": "python-web-scrapping-of-reactjs-page-in-the-nut-shell",
-    "tracking_id": "MyjBX6vaR7yUCEC2iUstyA",
-    "headline": "Learning by doing",
-    "caption_locales": [{
-        "_class": "locale",
-        "locale": "en_US",
-        "title": "English (US)",
-        "english_title": "English (US)"
-    }],
-    "discount": null,
-    "avg_rating": 0,
-    "rating": 0,
-    "num_reviews": 0,
-    "is_wishlisted": false,
-    "num_published_lectures": 15,
-    "image_100x100": "https://img-b.udemycdn.com/course/100x100/4440262_ebe6.jpg",
-    "is_in_user_subscription": false,
-    "locale": {
-        "_class": "locale",
-        "locale": "en_US",
-        "title": "English (US)",
-        "english_title": "English (US)",
-        "simple_english_title": "English"
+  'count': 1,
+  'next': null,
+  'previous': null,
+  'results': [{
+    '_class': 'course',
+    'id': 4701294,
+    'title': 'Customer Support in The Modern World – Crash Course',
+    'url': '/course/customer-support-in-the-modern-world-crash-course/',
+    'is_paid': true,
+    'price': '€29.99',
+    'price_detail': {
+      'amount': 29.99,
+      'currency': 'EUR',
+      'price_string': '€29.99',
+      'currency_symbol': '€'
     },
-    "caption_languages": ["English [Auto]"],
-    "instructional_level_simple": "All Levels",
-    "content_info": "2 total hours",
-    "buyable_object_type": "course",
-    "badges": [],
-    "is_recently_published": true,
-    "learn_url": "/course/python-web-scrapping-of-reactjs-page-in-the-nut-shell/learn/",
-    "predictive_score": null,
-    "relevancy_score": null,
-    "input_features": null,
-    "lecture_search_result": null,
-    "curriculum_lectures": [],
-    "order_in_results": null,
-    "curriculum_items": [],
-    "instructor_name": null
-}]
+    'price_serve_tracking_id': 'xbv5v02JQ0uiN8cMnsShYw',
+    'visible_instructors': [{
+      '_class': 'user',
+      'id': 195743536,
+      'title': 'Skilled Three',
+      'name': 'Skilled',
+      'display_name': 'Skilled Three',
+      'job_title': 'Growth focused guys with great insight into IT technologies,',
+      'image_50x50': 'https://img-b.udemycdn.com/user/50x50/195743536_634f_3.jpg',
+      'image_100x100': 'https://img-b.udemycdn.com/user/100x100/195743536_634f_3.jpg',
+      'initials': 'ST',
+      'url': '/user/skilled-three/'
+    }],
+    'image_125_H': 'https://img-b.udemycdn.com/course/125_H/4701294_4033_3.jpg',
+    'image_240x135': 'https://img-b.udemycdn.com/course/240x135/4701294_4033_3.jpg',
+    'is_practice_test_course': false,
+    'image_480x270': 'https://img-b.udemycdn.com/course/480x270/4701294_4033_3.jpg',
+    'published_title': 'customer-support-in-the-modern-world-crash-course',
+    'tracking_id': 'Di0zzdqMSDerOF5cIbVpJA',
+    'headline': 'All you need to know to improve your communication skills & security awareness for great customer support.',
+    'caption_locales': [{
+      '_class': 'locale',
+      'locale': 'en_US',
+      'title': 'English (US)',
+      'english_title': 'English (US)'
+    }],
+    'discount': {
+      'price_serve_tracking_id': 'xbv5v02JQ0uiN8cMnsShYw',
+      'price': {
+        'amount': 11.99,
+        'currency': 'EUR',
+        'price_string': '€11.99',
+        'currency_symbol': '€'
+      },
+      'list_price': {
+        'amount': 29.99,
+        'currency': 'EUR',
+        'price_string': '€29.99',
+        'currency_symbol': '€'
+      },
+      'saving_price': {
+        'amount': 18.0,
+        'currency': 'EUR',
+        'price_string': '€18.00',
+        'currency_symbol': '€'
+      },
+      'has_discount_saving': true,
+      'discount_percent': 60,
+      'discount_percent_for_display': 60,
+      'buyable': {
+        'id': 4701294,
+        'type': 'course'
+      },
+      'campaign': {
+        'code': 'ST8MT61422',
+        'end_time': '2022-06-16T22:00:00Z',
+        'is_instructor_created': false,
+        'is_public': true,
+        'start_time': '2022-06-14T07:00:00Z',
+        'campaign_type': 'deal',
+        'uses_remaining': null,
+        'maximum_uses': null,
+        'show_code': true
+      },
+      'code': 'ST8MT61422',
+      'is_public': true
+    },
+    'avg_rating': 0,
+    'rating': 0,
+    'num_reviews': 0,
+    'is_wishlisted': false,
+    'num_published_lectures': 20,
+    'image_100x100': 'https://img-b.udemycdn.com/course/100x100/4701294_4033_3.jpg',
+    'is_in_user_subscription': false,
+    'locale': {
+      '_class': 'locale',
+      'locale': 'en_US',
+      'title': 'English (US)',
+      'english_title': 'English (US)',
+      'simple_english_title': 'English'
+    },
+    'caption_languages': ['English'],
+    'instructional_level_simple': 'All Levels',
+    'content_info': '1 total hour',
+    'buyable_object_type': 'course',
+    'badges': [],
+    'is_recently_published': true,
+    'learn_url': '/course/customer-support-in-the-modern-world-crash-course/learn/',
+    'is_in_premium': false,
+    'predictive_score': null,
+    'relevancy_score': null,
+    'input_features': null,
+    'lecture_search_result': null,
+    'curriculum_lectures': [],
+    'order_in_results': null,
+    'curriculum_items': [],
+    'instructor_name': null
+  }]
+}];
 
-export {courses};
+export { courses };
