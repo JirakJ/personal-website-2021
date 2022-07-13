9d52c850d71013e315fa81f6cf9d37b8fbda8d56
Ing. Jakub Jirák <jakub@jakubjirak.com>
2022-07-13 11:05:54 +0200

    Fix blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -9,7 +9,7 @@ const renderBlogs = () => {
 
   useEffect(async () => {
     const result = await axios(
-      'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@skilledthree',
+      'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@jakubjirak',
     );
     setData(result.data);
   }, []);
