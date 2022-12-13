9531d8e1e1f86641991224846d07fc96ebc79bc1
Jakub Jirák <jakub@jakubjirak.com>
2022-12-13 09:52:05 +0100

    Update contact icons

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,6 +1,4 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
@@ -11,16 +9,6 @@ const data = [
     link: 'https://github.com/JirakJ',
     label: 'Github',
     icon: faGithub,
-  },
-  {
-    link: 'https://www.facebook.com/jjirak/',
-    label: 'Facebook',
-    icon: faFacebookF,
-  },
-  {
-    link: 'https://www.instagram.com/jirakjakub/',
-    label: 'Instagram',
-    icon: faInstagram,
   },
   {
     link: 'https://www.linkedin.com/in/jirakj/',
