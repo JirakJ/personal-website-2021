3ff02e5941b54a6071a3a42066966cd74c886e86
Jakub Jirák <jakub@jakubjirak.com>
2023-01-07 20:04:49 +0100

    Add YouTube section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Route,
 } from 'react-router-dom';
 import Main from './layouts/Main'; // fallback for lazy pages
-import './static/css/main.scss'; // All of our styles
+import './static/css/main.scss';
 
 const { PUBLIC_URL } = process.env;
 
@@ -20,6 +20,7 @@ const NotFound = lazy(() => import('./pages/NotFound'));
 const Projects = lazy(() => import('./pages/Projects'));
 const Resume = lazy(() => import('./pages/Resume'));
 const Blog = lazy(() => import('./pages/Blog'));
+const YouTube = lazy(() => import('./pages/YouTube'));
 
 const App = () => (
   <BrowserRouter basename={PUBLIC_URL}>
@@ -31,6 +32,7 @@ const App = () => (
         <Route path="/contact" component={Contact} />
         <Route path="/resume" component={Resume} />
         <Route path="/blog" component={Blog} />
+        <Route path="/youtube" component={YouTube} />
         <Redirect to="/" />
         <Route component={NotFound} status={404} />
       </Switch>
---
+++ src/pages/YouTube.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import Main from '../layouts/Main';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Post from '../components/YouTube/Post';
+import PropTypes from "prop-types";
+
+const renderVideos = () => {
+  const [data, setData] = useState({ videos: [] });
+
+  useEffect(async () => {
+    const result = await axios(
+      'https://api.rss2json.com/v1/api.json?rss_url=https://www.youtube.com/feeds/videos.xml?channel_id=UC0osYUKB5oePEdYRsamlhSw',
+    );
+    setData(result.data);
+  }, []);
+
+  if (data.items) {
+    return <article className="blog-post" id="blog">
+      <header>
+        <div className="title">
+          <h2 data-testid="heading"><Link to="/blog">YouTube</Link></h2>
+          <p>Jakub Jirák - YouTube feed</p>
+        </div>
+      </header>
+          {data.items.map((video) => (
+            <Post
+              data={video}
+              key={`${post.guid}`}
+            />
+          ))}
+    </article>
+  }
+};
+
+const YouTube = () => (
+  <Main
+    title="YouTube"
+    description=""
+  >
+        {renderVideos()}
+  </Main>
+  );
+
+YouTube.propTypes = {
+  fullPage: PropTypes.bool,
+};
+
+YouTube.defaultProps = {
+  fullPage: false,
+};
+
+export default YouTube;
+
+
